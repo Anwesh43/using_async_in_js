159f5de13d01c4d7893a2b2bc9dfeed7f20d7b27
Anwesh43 <anweshthecool0@gmail.com>
2017-05-25 15:54:55 +0530

    using AsyncBlock in timeout

--- public/async_timeout.js
+++ public/async_timeout.js
@@ -1,3 +1,4 @@
+const asyncTimeoutBlock = new AsyncBlock(200,200)
 function sendAfterTwoSeconds(n) {
     return new Promise((resolve)=>{
         setTimeout(()=>{
@@ -11,7 +12,9 @@ async function addNumbers(a,b) {
     console.log(newA)
     return newA + newB
 }
+asyncTimeoutBlock.startLoading()
 addNumbers(10,20).then((v)=>{
     console.log(v)
+    asyncTimeoutBlock.setValue(v)
 })
 //console.log(a)
